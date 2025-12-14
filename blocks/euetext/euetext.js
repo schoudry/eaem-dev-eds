88793a746d973322b5a2c7e8bf5a533e3b0eeb79
Sreekanth Choudry Nalabotu <nalabotu@adobe.com>
2025-12-14 15:50:29 -0600

    Update euetext.js

--- blocks/euetext/euetext.js
+++ blocks/euetext/euetext.js
@@ -1,3 +1,10 @@
 export default async function decorate(block) {
-    console.log("------> Sreek block: ", block);
+    const links = block.querySelectorAll('a');
+    
+    links.forEach(link => {
+        const href = link.getAttribute('href');
+        if (href && href.includes('open_in_new_tab=true')) {
+            link.setAttribute('target', '_blank');
+        }
+    });
 }

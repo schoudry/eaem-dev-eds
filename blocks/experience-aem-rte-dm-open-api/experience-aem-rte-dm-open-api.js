ec143c501b81c235d6c4fc6941b4db4a84a99cf0
Sreekanth Choudry Nalabotu <nalabotu@adobe.com>
2025-12-25 11:25:33 -0500

    changes

--- blocks/experience-aem-rte-dm-open-api/experience-aem-rte-dm-open-api.js
+++ blocks/experience-aem-rte-dm-open-api/experience-aem-rte-dm-open-api.js
@@ -1,7 +1,11 @@
 export default async function decorate(block) { 
-  // Remove button-container class from p elements
+  // Remove button-container class from p elements and remove classes from anchor tags inside
   const buttonContainers = block.querySelectorAll('p.button-container');
   buttonContainers.forEach(p => {
-    p.classList.remove('button-container');
+    p.className = '';
+    const anchor = p.querySelector('a');
+    if (anchor) {
+      anchor.className = '';
+    }
   });
 }

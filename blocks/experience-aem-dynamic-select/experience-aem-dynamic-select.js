27198e4afe1de70f28dcdf26feb2d6961bce30d5
Sreekanth Choudry Nalabotu <nalabotu@adobe.com>
2025-12-24 19:23:39 -0500

    changes

--- blocks/experience-aem-dynamic-select/experience-aem-dynamic-select.js
+++ blocks/experience-aem-dynamic-select/experience-aem-dynamic-select.js
@@ -1,16 +1,11 @@
 export default async function decorate(block) {
-  console.log("eaem dynamic select--------", block);
-  
-  // Find all button-container elements
   const buttonContainers = block.querySelectorAll('.button-container');
   
   buttonContainers.forEach(container => {
-    // Get the text content from the link inside
     const link = container.querySelector('a');
     if (link) {
       const textContent = link.textContent;
-      // Replace the entire container content with just the text
-      container.innerHTML = textContent;
+      container.innerHTML = "Selected root folder: " + textContent;
       container.classList.remove('button-container');
     }
   });

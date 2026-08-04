ac07969e84af07510fbbbc1576d7ca1a24fd8664
Sreekanth Choudry Nalabotu <nalabotu@adobe.com>
2026-08-04 09:50:13 -0500

    trying links

--- blocks/euetext/euetext.js
+++ blocks/euetext/euetext.js
@@ -4,6 +4,8 @@ export default async function decorate(block) {
     }
     
     const links = block.querySelectorAll('a');
+
+    //try links
     
     links.forEach(link => {
         const href = link.getAttribute('href');

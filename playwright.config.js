61188ce232c4ca8b355aad76fff1cc1cf42d1d33
Sreekanth Choudry Nalabotu <nalabotu@adobe.com>
2026-08-04 10:01:38 -0500

    changes

--- playwright.config.js
+++ playwright.config.js
@@ -6,36 +6,36 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
-  
+
   /* Run tests in files in parallel */
   fullyParallel: true,
-  
+
   /* Fail the build on CI if you accidentally left test.only in the source code */
   forbidOnly: !!process.env.CI,
-  
+
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  
+
   /* Opt out of parallel tests on CI */
   workers: process.env.CI ? 1 : undefined,
-  
+
   /* Reporter to use */
   reporter: [
     ['html'],
-    ['list']
+    ['list'],
   ],
-  
+
   /* Shared settings for all the projects below */
   use: {
     /* Base URL for aem proxy (local development) */
     baseURL: process.env.BASE_URL || 'http://localhost:3000',
-    
+
     /* Collect trace when retrying the failed test */
     trace: 'on-first-retry',
-    
+
     /* Screenshot on failure */
     screenshot: 'only-on-failure',
-    
+
     /* Video on failure */
     video: 'retain-on-failure',
   },
@@ -51,7 +51,7 @@ export default defineConfig({
     {
       name: 'Mobile Chrome',
       use: { ...devices['Pixel 5'] },
-    }
+    },
   ],
 
   /* Run your local dev server before starting the tests */
@@ -62,4 +62,3 @@ export default defineConfig({
     timeout: 120 * 1000,
   },
 });
-
